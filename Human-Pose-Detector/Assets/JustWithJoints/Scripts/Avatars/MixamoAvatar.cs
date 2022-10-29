bfeba8e1245080344784e3ccd1db461a0b78a451
junkim100 <junkim100@gmail.com>
2022-10-29 16:32:56 -0400

    Accurately follows webcam movement real-time

---
+++ Human-Pose-Detector/Assets/JustWithJoints/Scripts/Avatars/MixamoAvatar.cs
@@ -0,0 +1,191 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace JustWithJoints.Avatars
+{
+    public class MixamoAvatar : MonoBehaviour
+    {
+        public GameObject PointListAnnotation;
+        public string DataPath = "Assets/Avatar/movementUpdate.txt";
+        public GameObject MotionProvider;
+        public bool EnableRetargetting = true;
+        public bool RetargetTranslation = true;
+
+        List<GameObject> joints_ = new List<GameObject>();
+        List<GameObject> bones_ = new List<GameObject>();
+        Vector3[] correctionRightEulers = new Vector3[13];
+
+        
+        private int frame = 0;
+        private string[] joints = {
+                                "rtibia", //26
+                                "rfemur", //24
+                                "rhipjoint", //24
+                                "lhipjoint", //25
+                                "lfemur", //25
+                                "ltibia", //27 //Knee
+                                "rwrist", //16
+                                "rhumerus", //14
+                                "rclavicle", //12
+                                "lclavicle", //13
+                                "lhumerus", //15
+                                "lwrist", //17
+                                "thorax", // 12+13+24+25/4
+                                "head" //1
+                            };
+
+        void Start()
+        {
+            var root = gameObject;
+
+            string prefix = "";
+            for (int i = 0; i < root.gameObject.transform.childCount; i++)
+            {
+                var child = root.gameObject.transform.GetChild(i);
+                if (child.name.EndsWith("Hips"))
+                {
+                    prefix = child.name.Substring(0, child.name.Length - 4);
+                }
+            }
+
+            var hips = root.gameObject.transform.Find(prefix + "Hips").gameObject;
+            var rightUpLeg = hips.transform.Find(prefix + "RightUpLeg").gameObject;
+            var rightLeg = rightUpLeg.transform.Find(prefix + "RightLeg").gameObject;
+            var rightFoot = rightLeg.transform.Find(prefix + "RightFoot").gameObject;
+            var rightToeBase = rightFoot.transform.Find(prefix + "RightToeBase").gameObject;
+            var leftUpLeg = hips.transform.Find(prefix + "LeftUpLeg").gameObject;
+            var leftLeg = leftUpLeg.transform.Find(prefix + "LeftLeg").gameObject;
+            var leftFoot = leftLeg.transform.Find(prefix + "LeftFoot").gameObject;
+            var leftToeBase = leftFoot.transform.Find(prefix + "LeftToeBase").gameObject;
+            var spine = hips.transform.Find(prefix + "Spine").gameObject;
+            var spine1 = spine.transform.Find(prefix + "Spine1").gameObject;
+            var spine2 = spine1.transform.Find(prefix + "Spine2").gameObject;
+            var leftShoulder = spine2.transform.Find(prefix + "LeftShoulder").gameObject;
+            var leftArm = leftShoulder.transform.Find(prefix + "LeftArm").gameObject;
+            var leftForeArm = leftArm.transform.Find(prefix + "LeftForeArm").gameObject;
+            var leftHand = leftForeArm.transform.Find(prefix + "LeftHand").gameObject;
+            var rightShoulder = spine2.transform.Find(prefix + "RightShoulder").gameObject;
+            var rightArm = rightShoulder.transform.Find(prefix + "RightArm").gameObject;
+            var rightForeArm = rightArm.transform.Find(prefix + "RightForeArm").gameObject;
+            var rightHand = rightForeArm.transform.Find(prefix + "RightHand").gameObject;
+            var neck = spine2.transform.Find(prefix + "Neck").gameObject;
+            var head = neck.transform.Find(prefix + "Head").gameObject;
+
+            bones_.Clear();
+            bones_.AddRange(new GameObject[]
+            {
+                hips,
+                rightUpLeg,
+                rightLeg,
+                leftUpLeg,
+                leftLeg,
+                spine,
+                leftShoulder,
+                leftArm,
+                leftForeArm,
+                rightShoulder,
+                rightArm,
+                rightForeArm,
+                neck,
+            });
+
+
+            var invRoot = Quaternion.Inverse(gameObject.transform.rotation);
+            string[] lines = System.IO.File.ReadAllLines("Assets/JustWithJoints/MotionData/unitychan_inv_tpose.txt");
+            for (int i = 0; i < correctionRightEulers.Length; i++)
+            {
+                var tokens = lines[i].Split(' ');
+                float x = float.Parse(tokens[0]);
+                float y = float.Parse(tokens[1]);
+                float z = float.Parse(tokens[2]);
+                var inv = Quaternion.Euler(x, y, z);
+                var rot = inv * bones_[i].transform.rotation * invRoot;
+                correctionRightEulers[i] = rot.eulerAngles;
+            }
+        }
+
+        void LateUpdate()
+        {
+            Transform[] allChildren = PointListAnnotation.transform.GetComponentsInChildren<Transform>();
+            movementUpdate(DataPath, allChildren);
+
+            // Get pose
+            Core.Pose pose = null;
+            if (MotionProvider)
+            {
+                var component = MotionProvider.GetComponent<CMUMotionPlayer>();
+                if (component)
+                {
+                    pose = component.GetCurrentPose();
+                }
+            }
+
+            // Retarget
+            if (pose != null)
+            {
+                // Retarget positions
+                if (RetargetTranslation)
+                {
+                    bones_[0].transform.position = (pose.Positions[2] + pose.Positions[3]) * 0.5f + gameObject.transform.position;
+                }
+
+                if (EnableRetargetting)
+                {
+                    // Retarget rotations
+                    for (int i = 0; i < 13; i++)
+                    {
+                        int boneIndex = i;
+                        if (i == (int)Core.BoneType.Trans)
+                        {
+                            // Use spine as root of avatar
+                            boneIndex = (int)Core.BoneType.Spine;
+                        }
+                        if (i == (int)Core.BoneType.Spine)
+                        {
+                            // Skip spine
+                            continue;
+                        }
+                        if (i == (int)Core.BoneType.RightShoulder || i == (int)Core.BoneType.LeftShoulder)
+                        {
+                            // Skip shoulders
+                            continue;
+                        }
+                        bones_[i].transform.rotation =
+                            gameObject.transform.rotation *
+                            pose.Rotations[boneIndex] *
+                            Quaternion.Euler(correctionRightEulers[i]);
+                    }
+                }
+            }
+        }
+
+
+        private void movementUpdate(string DataPath, Transform[] allChildren) {
+            // Clear text file
+            var writer = new System.IO.StreamWriter(DataPath, false);
+            writer.WriteLine("");
+
+            if (allChildren.Length >= 10) {
+                writer.WriteLine(string.Format("frame: {0}", frame));
+                writer.WriteLine(string.Format("\t{0}: {1} {2} {3}", joints[0], allChildren[26].transform.localPosition.x, allChildren[26].transform.localPosition.y, allChildren[26].transform.localPosition.z));
+                writer.WriteLine(string.Format("\t{0}: {1} {2} {3}", joints[1], allChildren[24].transform.localPosition.x, allChildren[24].transform.localPosition.y, allChildren[24].transform.localPosition.z));
+                writer.WriteLine(string.Format("\t{0}: {1} {2} {3}", joints[2], allChildren[24].transform.localPosition.x, allChildren[24].transform.localPosition.y, allChildren[24].transform.localPosition.z));
+                writer.WriteLine(string.Format("\t{0}: {1} {2} {3}", joints[3], allChildren[25].transform.localPosition.x, allChildren[25].transform.localPosition.y, allChildren[25].transform.localPosition.z));
+                writer.WriteLine(string.Format("\t{0}: {1} {2} {3}", joints[4], allChildren[25].transform.localPosition.x, allChildren[25].transform.localPosition.y, allChildren[25].transform.localPosition.z));
+                writer.WriteLine(string.Format("\t{0}: {1} {2} {3}", joints[5], allChildren[27].transform.localPosition.x, allChildren[27].transform.localPosition.y, allChildren[27].transform.localPosition.z));
+                writer.WriteLine(string.Format("\t{0}: {1} {2} {3}", joints[6], allChildren[16].transform.localPosition.x, allChildren[16].transform.localPosition.y, allChildren[16].transform.localPosition.z));
+                writer.WriteLine(string.Format("\t{0}: {1} {2} {3}", joints[7], allChildren[14].transform.localPosition.x, allChildren[14].transform.localPosition.y, allChildren[14].transform.localPosition.z));
+                writer.WriteLine(string.Format("\t{0}: {1} {2} {3}", joints[8], allChildren[12].transform.localPosition.x, allChildren[12].transform.localPosition.y, allChildren[12].transform.localPosition.z));
+                writer.WriteLine(string.Format("\t{0}: {1} {2} {3}", joints[9], allChildren[13].transform.localPosition.x, allChildren[13].transform.localPosition.y, allChildren[13].transform.localPosition.z));
+                writer.WriteLine(string.Format("\t{0}: {1} {2} {3}", joints[10], allChildren[15].transform.localPosition.x, allChildren[15].transform.localPosition.y, allChildren[15].transform.localPosition.z));
+                writer.WriteLine(string.Format("\t{0}: {1} {2} {3}", joints[11], allChildren[17].transform.localPosition.x, allChildren[17].transform.localPosition.y, allChildren[17].transform.localPosition.z));
+                writer.WriteLine(string.Format("\t{0}: {1} {2} {3}", joints[12], (allChildren[12].transform.localPosition.x+allChildren[13].transform.localPosition.x+allChildren[24].transform.localPosition.x+allChildren[25].transform.localPosition.x)/4, (allChildren[12].transform.localPosition.y+allChildren[13].transform.localPosition.y+allChildren[24].transform.localPosition.y+allChildren[25].transform.localPosition.y)/4, (allChildren[12].transform.localPosition.z+allChildren[13].transform.localPosition.z+allChildren[24].transform.localPosition.z+allChildren[25].transform.localPosition.z)/4));
+                writer.WriteLine(string.Format("\t{0}: {1} {2} {3}", joints[13], allChildren[1].transform.localPosition.x, allChildren[1].transform.localPosition.y, allChildren[1].transform.localPosition.z));
+                frame++;
+            }
+
+            writer.Close();
+        }
+    }
+}
--- Human-Pose-Detector/Assets/Scripts/animateAvatar.cs
+++ Human-Pose-Detector/Assets/Scripts/animateAvatar.cs
@@ -4,65 +4,28 @@ using UnityEngine;
 
 public class animateAvatar : MonoBehaviour
 {
+    public GameObject Model;
     public GameObject PointListAnnotation;
-    public GameObject Avatar;
-
-    void Start()
-    {
-        // Transform[] bodyParts = Avatar.transform.GetComponentsInChildren<Transform>();
-        // for (int i = 0; i < bodyParts.Length; i++) {
-        //     Debug.Log(bodyParts[i].name);
-        // }
-
-        // // Left Leg
-        // Debug.Log(bodyParts[4].name);
-        // Debug.Log(bodyParts[5].name);
-        // Debug.Log(bodyParts[6].name);
-        // Debug.Log(bodyParts[8].name);
-
-        // // Right Leg
-        // Debug.Log(bodyParts[9].name);
-        // Debug.Log(bodyParts[10].name);
-        // Debug.Log(bodyParts[11].name);
-        // Debug.Log(bodyParts[13].name);
-
-        // // Left Arm
-        // Debug.Log(bodyParts[18].name);
-        // Debug.Log(bodyParts[19].name);
-        // Debug.Log(bodyParts[20].name);
-
-        // // Left Finger
-        // Debug.Log(bodyParts[21].name);
-        // Debug.Log(bodyParts[37].name);
-
-        // // Right Arm
-        // Debug.Log(bodyParts[45].name);
-        // Debug.Log(bodyParts[46].name);
-        // Debug.Log(bodyParts[47].name);
-
-        // // Right Finger
-        // Debug.Log(bodyParts[48].name);
-        // Debug.Log(bodyParts[64].name);
-
-        // //Head
-        // Debug.Log(bodyParts[42].name);
-    }
 
     void Update()
     {
-        Transform[] bodyParts = Avatar.transform.GetComponentsInChildren<Transform>();
-
+        Transform[] bodyParts = Model.transform.GetComponentsInChildren<Transform>();
         Transform[] allChildren = PointListAnnotation.transform.GetComponentsInChildren<Transform>();
+
         // for (int i = 0; i < allChildren.Length; i++) {
         //     Debug.Log(allChildren[i].name);
         // }
 
-        if (allChildren.Length != 1)
-            updateBodyParts(bodyParts, allChildren);
+        // if (allChildren.Length != 1)
+        //     placeBodyParts(bodyParts, allChildren);
 
+
+        // bodyParts[0].transform.localPosition = (allChildren[24].transform.localPosition + allChildren[25].transform.localPosition)/2;
+        // Debug.Log((allChildren[24].transform.localPosition + allChildren[25].transform.localPosition)/2);
+        Debug.Log(bodyParts[0].transform.position);
     }
 
-    private void updateBodyParts(Transform[] bodyParts, Transform[] allChildren) {
+    private void placeBodyParts(Transform[] bodyParts, Transform[] allChildren) {
         // Body
         if (bodyParts[42].transform.position != null) {
             bodyParts[3].transform.position = new Vector3(bodyParts[42].transform.position.x, bodyParts[42].transform.position.y-20.0f, bodyParts[42].transform.position.z);
@@ -77,10 +40,18 @@ public class animateAvatar : MonoBehaviour
         bodyParts[19].transform.position = allChildren[15].transform.position;
         bodyParts[20].transform.position = allChildren[17].transform.position;
 
+        rotateBodyParts(allChildren[13], allChildren[15], bodyParts[18]);
+        rotateBodyParts(allChildren[15], allChildren[17], bodyParts[19]);
+        rotateBodyParts(allChildren[17], allChildren[21], bodyParts[20]);
+
         // Right Arm
         bodyParts[45].transform.position = allChildren[12].transform.position;
         bodyParts[46].transform.position = allChildren[14].transform.position;
         bodyParts[47].transform.position = allChildren[16].transform.position;
+
+        rotateBodyParts(allChildren[12], allChildren[14], bodyParts[45]);
+        rotateBodyParts(allChildren[14], allChildren[16], bodyParts[46]);
+        rotateBodyParts(allChildren[16], allChildren[20], bodyParts[47]);
 
         // Right Leg
         bodyParts[9].transform.position = allChildren[24].transform.position;
@@ -88,14 +59,35 @@ public class animateAvatar : MonoBehaviour
         bodyParts[11].transform.position = allChildren[28].transform.position;
         bodyParts[13].transform.position = allChildren[32].transform.position;
 
+        rotateBodyParts(allChildren[24], allChildren[26], bodyParts[9]);
+        rotateBodyParts(allChildren[26], allChildren[28], bodyParts[10]);
+        rotateBodyParts(allChildren[28], allChildren[32], bodyParts[11]);
+        // rotateBodyParts(allChildren[32], allChildren[20], bodyParts[13]);
+
         // // Left Leg
         bodyParts[4].transform.position = allChildren[25].transform.position;
         bodyParts[5].transform.position = allChildren[27].transform.position;
         bodyParts[6].transform.position = allChildren[29].transform.position;
         bodyParts[8].transform.position = allChildren[33].transform.position;
 
+        rotateBodyParts(allChildren[25], allChildren[27], bodyParts[4]);
+        rotateBodyParts(allChildren[27], allChildren[29], bodyParts[5]);
+        rotateBodyParts(allChildren[29], allChildren[33], bodyParts[6]);
+        // rotateBodyParts(allChildren[33], allChildren[20], bodyParts[8]);
+
         // Head
         bodyParts[42].transform.position = allChildren[1].transform.position;
+    }
+
+    private void rotateBodyParts(Transform part1, Transform part2, Transform bodyPart) {
+        Vector3 point1 = part1.position;
+        Vector3 point2 = part2.position;
+        Vector3 rotationVector = point1 - point2;
+        Debug.Log(rotationVector);
+        // rotationVector.x = rotationVector.x*2;
+        Debug.Log(Quaternion.LookRotation(rotationVector));
+        bodyPart.rotation = Quaternion.LookRotation(rotationVector);
+        bodyPart.Rotate(270, 0, 0);
     }
 
 }
